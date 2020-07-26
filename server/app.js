70462a02f46591086069c277c15d00d0324c2add
xelinel32 <artemsedlyar99@gmail.com>
2020-07-26 14:53:08 +0300

    test vue sockets on client

--- server/app.js
+++ server/app.js
@@ -4,9 +4,17 @@ const io = require('socket.io')(server)
 
 io.on('connection', socket => {
   console.log('IO Connected')
+
+  socket.on('CREATE_MESSAGE', data => {
+    setTimeout(() => {
+      socket.emit('NEW_MESSAGE', {
+        text: data.text + ' SERVER',
+      })
+    }, 500)
+  })
 })
 
 module.exports = {
   app,
-  server
+  server,
 }
