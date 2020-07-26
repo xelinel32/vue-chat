1ff6f9633db83573460f9eb642b16bd4af5fa293
xelinel32 <artemsedlyar99@gmail.com>
2020-07-26 16:16:34 +0300

    create mutation: clear_data

--- store/index.js
+++ store/index.js
@@ -1,10 +1,15 @@
 export const state = () => ({
   user: {},
+  messages: [],
 })
 
 export const mutations = {
   SET_USER(state, user) {
     state.user = user
+  },
+  CLEAR_DATA(state) {
+    state.user = {}
+    state.messages = []
   },
 }
 
