e2a10ab810bb290051d534afeffb2999e836008a
xelinel32 <artemsedlyar99@gmail.com>
2020-07-26 16:15:44 +0300

    update default layout for chat page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,62 @@
 <template>
-  <v-app app>
-    <nuxt />
+  <v-app app dark>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list subheader>
+        <v-subheader>Users list in chat</v-subheader>
+        <v-list-tile v-for="u in users" :key="u.id" @click.prevent="">
+          <v-list-tile-content>
+            <v-list-tile-title>{{ u.name }}</v-list-tile-title>
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-icon :color="u.id === 2 ? 'primary' : 'grey'"
+              >chat_bubble</v-icon
+            >
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app>
+      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-btn icon @click="exit">
+        <v-icon>arrow_back</v-icon>
+      </v-btn>
+      <v-toolbar-title>Number of the room {{ user.room }}</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <div>
+        <nuxt />
+      </div>
+    </v-content>
   </v-app>
 </template>
 
 <script>
-export default {}
+import { mapState, mapMutations } from 'vuex'
+export default {
+  data() {
+    return {
+      drawer: true,
+      users: [
+        {
+          id: 1,
+          name: 'Artem',
+        },
+        {
+          id: 2,
+          name: 'Vlad',
+        },
+      ],
+    }
+  },
+  computed: {
+    ...mapState(['user']),
+  },
+  methods: {
+    ...mapMutations(['CLEAR_DATA']),
+    exit() {
+      this.$router.push('/?message=leftChat')
+      this.CLEAR_DATA()
+    },
+  },
+}
 </script>
