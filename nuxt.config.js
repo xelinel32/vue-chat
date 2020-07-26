dba2972fcfed47836cf10419b873c3a2fe52c1d6
xelinel32 <artemsedlyar99@gmail.com>
2020-07-26 14:49:31 +0300

    registered socket plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,8 @@
-const pkg = require("./package");
-const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin");
+const pkg = require('./package')
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
 
 module.exports = {
-  mode: "universal",
+  mode: 'universal',
 
   /*
    ** Headers of the page
@@ -10,54 +10,54 @@ module.exports = {
   head: {
     title: pkg.name,
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: pkg.description }
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description },
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
-        rel: "stylesheet",
+        rel: 'stylesheet',
         href:
-          "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"
-      }
-    ]
+          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons',
+      },
+    ],
   },
 
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: "#fff" },
+  loading: { color: '#fff' },
 
   /*
    ** Global CSS
    */
-  css: ["~/assets/style/app.styl"],
+  css: ['~/assets/style/app.styl'],
 
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ["@/plugins/vuetify"],
+  plugins: ['@/plugins/vuetify', { src: '@/plugins/socket', ssr: false }],
 
   /*
    ** Nuxt.js modules
    */
-  modules: ["@nuxtjs/pwa"],
+  modules: ['@nuxtjs/pwa'],
 
   /*
    ** Build configuration
    */
   build: {
-    transpile: ["vuetify/lib"],
+    transpile: ['vuetify/lib'],
     plugins: [new VuetifyLoaderPlugin()],
     loaders: {
       stylus: {
-        import: ["~assets/style/variables.styl"]
-      }
+        import: ['~assets/style/variables.styl'],
+      },
     },
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
-  }
-};
+    extend(config, ctx) {},
+  },
+}
