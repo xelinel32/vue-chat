3c8fbfeebb5591c18780b909fb627bd9626ad1cf
xelinel32 <artemsedlyar99@gmail.com>
2020-07-26 14:50:08 +0300

    create login page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,72 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6></v-flex>
+    <v-flex xs12 sm8>
+      <v-card min-width="600px">
+        <v-card-title>
+          <h1>Vue chat</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field
+              v-model="name"
+              :counter="16"
+              :rules="nameRules"
+              label="Your name"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="room"
+              :rules="roomRules"
+              label="Write room"
+              required
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn :disabled="!valid" color="primary" @click="submit">
+            Login
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
   </v-layout>
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
 export default {
-  components: {}
-};
+  layout: 'empty',
+  head: {
+    title: 'Welcome to Vue chat',
+  },
+  data: () => ({
+    valid: true,
+    name: '',
+    nameRules: [
+      v => !!v || 'Введите имя',
+      v => (v && v.length <= 10) || 'Имя не должно превышать 16 символов',
+    ],
+    room: '',
+    roomRules: [v => !!v || 'Введите комнату'],
+  }),
+  sockets: {
+    connect() {
+      console.log('socket connected')
+    },
+  },
+  methods: {
+    ...mapMutations(['SET_USER']),
+    submit() {
+      if (this.$refs.form.validate()) {
+        const user = {
+          name: this.name,
+          room: this.room,
+        }
+
+        this.SET_USER(user)
+        this.$router.push('/chat')
+      }
+    },
+  },
+}
 </script>
