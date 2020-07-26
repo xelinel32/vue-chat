2c0ebaa76e88e406f9ae860f9c18736333e79e21
xelinel32 <artemsedlyar99@gmail.com>
2020-07-26 14:50:19 +0300

    create room page

---
+++ pages/chat.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>CHAT Page {{ user.name }}</h1>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'chatPage',
+  middleware: ['chat'],
+  head() {
+    return {
+      title: `Room ${this.user.room}`,
+    }
+  },
+  computed: {
+    ...mapState(['user']),
+  },
+}
+</script>
+
+<style></style>
