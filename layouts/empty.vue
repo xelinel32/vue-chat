74d6ce5cd871ffe33ebafa4618ec3691653ed052
xelinel32 <artemsedlyar99@gmail.com>
2020-07-26 14:48:41 +0300

    create empty layout

---
+++ layouts/empty.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app app dark>
+    <v-content>
+      <v-container fluid>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
